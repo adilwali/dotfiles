c5394474b4087391f81a02e15925f70dd2891f2d
Adil Wali <adil@merit.me>
2018-04-07 14:05:22 -0700

    Add MongoDB

--- src/os/install/ubuntu/databases.sh
+++ src/os/install/ubuntu/databases.sh
@@ -4,3 +4,9 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
   sudo apt-key add -
 sudo apt-get update
 sudo apt-get install postgresql-10
+
+# MongoDB
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
