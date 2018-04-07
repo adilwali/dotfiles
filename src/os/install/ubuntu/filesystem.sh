866055db62da7ad33aa45558c24ad0ae676d7621
Adil Wali <adil@merit.me>
2018-04-07 15:29:17 -0700

    Refresh FSWatcher

--- src/os/install/ubuntu/filesystem.sh
+++ src/os/install/ubuntu/filesystem.sh
@@ -1,4 +1,5 @@
 echo 'fs.inotify.max_user_watches=524288' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
 
 # Find if it it's already there -- WIP
 #sed -i -e 's/^ *fs\.inotify\.max_user_watches\( \+B\)\? *$/fs.inotify.max_user_watches=524288/' "./sysctl.conf"
